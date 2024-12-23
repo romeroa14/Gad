a3d1146206e7a3ed58f7995579d63a2723104a59
Alfredo Romero <Alfredo Romero>
2024-12-23 16:51:33 -0400

    Otra config

--- app/Services/FacebookAds/FacebookAdsService.php
+++ app/Services/FacebookAds/FacebookAdsService.php
@@ -15,14 +15,29 @@ class FacebookAdsService
 
     public function __construct()
     {
-        $this->api = Api::init(
-            config('services.facebook.603275022244128'),
-            config('services.facebook.f54aa934d5b30c295299bb76390e3806'),
-            config('services.facebook.EAAIkrOlnGSABOZCzwgdzOdTXUkOYcm1snfCJ2nzIrnZC8vaCLe2PmdnCfl1GYfN30QgZA6aImNKrhbJqTZBAbtnLqbneaE4y8caKhfn887yylfAzEVv1VJB1fhNrDOjnNoZA2uNR6JYo0mAVZA7kfcl4VRDZAQhXJRmI6VgFv5OZB8CjjLBpztzDR3tJ6WtXrhz6lfG6gSFAqTWlSVwLqHH5JVQC2ZCiTwvNzx3wEhdVT')
-        );
-        
-        $adAccountId = config('services.facebook.933248667753162');
-        $this->adAccount = new AdAccount($adAccountId);
+        try {
+            // Inicializar la API con los valores correctos
+            $this->api = Api::init(
+                '603275022244128',                // Usar el valor directamente si no quieres usar env()
+                'f54aa934d5b30c295299bb76390e3806',
+                'EAAIkrOlnGSABOZCzwgdzOdTXUkOYcm1snfCJ2nzIrnZC8vaCLe2PmdnCfl1GYfN30QgZA6aImNKrhbJqTZBAbtnLqbneaE4y8caKhfn887yylfAzEVv1VJB1fhNrDOjnNoZA2uNR6JYo0mAVZA7kfcl4VRDZAQhXJRmI6VgFv5OZB8CjjLBpztzDR3tJ6WtXrhz6lfG6gSFAqTWlSVwLqHH5JVQC2ZCiTwvNzx3wEhdVT'
+            );
+            
+            // Obtener el ID de la cuenta de anuncios
+            $adAccountId = '933248667753162';     // Usar el valor directamente
+            
+            if (empty($adAccountId)) {
+                throw new \Exception('Facebook Ad Account ID no está configurado en el archivo .env');
+            }
+            
+            // Asegurarse de que el ID tenga el formato correcto
+            $accountId = strpos($adAccountId, 'act_') === 0 ? $adAccountId : 'act_' . $adAccountId;
+            $this->adAccount = new AdAccount($accountId);
+            
+        } catch (\Exception $e) {
+            Log::error('Error al inicializar FacebookAdsService: ' . $e->getMessage());
+            throw $e;
+        }
     }
 
     public function getCampaigns()
@@ -66,13 +81,13 @@ class FacebookAdsService
     public function getAccountInsights()
     {
         try {
-            $fields = 'impressions,clicks,spend';
+            $fields = 'impressions,clicks,spend,reach,frequency,cost_per_action_type,actions,ctr';
             $timeRange = [
                 'since' => '2024-01-01',
                 'until' => '2024-12-20'
             ];
 
-            $adAccountId = config('services.facebook.ad_account_id');
+            $adAccountId = str_replace('act_', '', config('services.facebook.ad_account_id'));
             
             $response = Http::withToken(config('services.facebook.access_token'))
                 ->get("https://graph.facebook.com/v21.0/act_{$adAccountId}/insights", [
@@ -89,7 +104,7 @@ class FacebookAdsService
 
     public function makeRequest($endpoint, $id)
     {
-        $response = Http::withToken(config('services.facebook.access_token'))
+        $response = Http::withToken(config('services.facebook.EAAIkrOlnGSABOZCzwgdzOdTXUkOYcm1snfCJ2nzIrnZC8vaCLe2PmdnCfl1GYfN30QgZA6aImNKrhbJqTZBAbtnLqbneaE4y8caKhfn887yylfAzEVv1VJB1fhNrDOjnNoZA2uNR6JYo0mAVZA7kfcl4VRDZAQhXJRmI6VgFv5OZB8CjjLBpztzDR3tJ6WtXrhz6lfG6gSFAqTWlSVwLqHH5JVQC2ZCiTwvNzx3wEhdVT'))
             ->get("https://graph.facebook.com/v18.0/{$id}/{$endpoint}");
 
         return $response->json();
@@ -104,7 +119,7 @@ class FacebookAdsService
     public function testToken()
     {
         try {
-            $response = Http::withToken(config('services.facebook.access_token'))
+            $response = Http::withToken(config('services.facebook.EAAIkrOlnGSABOZCzwgdzOdTXUkOYcm1snfCJ2nzIrnZC8vaCLe2PmdnCfl1GYfN30QgZA6aImNKrhbJqTZBAbtnLqbneaE4y8caKhfn887yylfAzEVv1VJB1fhNrDOjnNoZA2uNR6JYo0mAVZA7kfcl4VRDZAQhXJRmI6VgFv5OZB8CjjLBpztzDR3tJ6WtXrhz6lfG6gSFAqTWlSVwLqHH5JVQC2ZCiTwvNzx3wEhdVT'))
                 ->get('https://graph.facebook.com/v21.0/me');
             
             return $response->json();
